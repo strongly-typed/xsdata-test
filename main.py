ec5aa4488b005273052f81c5f86ef6907c93b2eb
Sascha Schade (strongly-typed) <stronglytyp3d@gmail.com>
2020-11-09 22:24:24 +0100

    Download test file and parse

---
+++ main.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from xsdata.formats.dataclass.parsers import XmlParser
+from xsdata.formats.dataclass.parsers.config import ParserConfig
+config = ParserConfig(fail_on_unknown_properties=True)
+parser = XmlParser(config=config)
+
+from btlx.www.design2machine.com.btlx import btlx_11
+
+fname = 'whole-log-wall.btlx'
+
+# Validate BTLx against XSD
+validate = True
+if validate:
+    from lxml import etree
+    from lxml import objectify
+    xmlschema_doc = etree.parse('btlx_11.xsd')
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    tree = objectify.parse(fname)
+    xmlschema.assertValid(tree)
+
+# Use xsd
+from btlx import btlx_11
+fpath = Path(fname)
+btlx = parser.from_path(fpath, btlx_11)
+
+print(btlx.project.name)
